99dd996d2bd24789c9fbbf5ba2c6dc39eb326e20
Ina Nuri Yohari <inanuriyohari@gmail.com>
2020-01-21 15:17:33 +0700

    about me fixed

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React from 'react';
-
+import Header from './component/Header';
+import About from './component/About';
+import ContactMe from './component/ContactMe';
+import "./index.css"
 
 function App() {
   return (
     <div>
-      <h1 className="title">Ina Nuri Yohari</h1>
+      <Header />
       <main className="profilePicture">
         <img src="https://www.shareicon.net/data/512x512/2016/09/15/829453_user_512x512.png" alt="image" class="profileImage"></img>
       </main>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      <h2 className="aboutMe">About Me</h2>
+      <About />
+      <About />
+
+      <ContactMe />
     </div>
   );
 }
---
+++ src/component/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import "../index.css"
+
+export default function Header() {
+    return (
+        <div>
+            <h1 className="title">Ina Nuri Yohari</h1>
+        </div>
+    )
+}
+
